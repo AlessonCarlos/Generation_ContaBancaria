50bcadf126c6f20d689cbdf1733c15176f6879de
AlessonCarlos <alesson.carlos5@gmail.com>
2025-12-08 14:28:09 -0300

    Inclusão da classe conta e pasta model, junto com a criação do objeto na classe Menu.ts

--- Menu.ts
+++ Menu.ts
@@ -1,14 +1,27 @@
 import readlinesync = require("readline-sync");
 import { colors } from './src/util/Colors';
+import { Conta } from './src/model/Conta';
+
 
 export function main() {
+
     let opcao: number;
+
+    // Objeto conta
+    const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
+    conta.visualizar();
+
+    conta.sacar(10500);
+    conta.visualizar();
+    
+    conta.depositar(5000);
+    conta.visualizar();
 
     while (true) {
         
         console.log(colors.bg.black, colors.fg.yellow,                                                                                                                                  "***************************************************");
         console.log("                                                     ");
-        console.log("                BANCO DO BRAZIL COM Z                ");
+        console.log("                BANCO DO BRAZIL COM Z                "); 
         console.log("                                                     ");
         console.log("*****************************************************");
         console.log("                                                     ");
